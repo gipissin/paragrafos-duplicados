76285b04704bbf2e0d6499df14c77d170ccae30a
Giovanna Vinturi <gigipissinatovinturi@hotmail.com>
2025-06-16 18:47:54 -0300

    Add files via upload

---
+++ Biblioteca/src/helpers.js
@@ -0,0 +1,19 @@
+function filtraOcorrencias(paragrafo) {
+    return Object.keys(paragrafo).filter(chave => paragrafo[chave] > 1);
+}
+
+function montaSaidaArquivo(listaPalavras) {
+    let textoFinal = '';
+
+    listaPalavras.forEach((paragrafo, indice) => {
+        const duplicadas = filtraOcorrencias(paragrafo);
+
+        if (duplicadas.length > 0) {
+            textoFinal += `palavras duplicadas no parágrafo ${indice + 1}: ${duplicadas.join(', ')}\n`;
+        }
+    });
+
+    return textoFinal;
+}
+
+export { montaSaidaArquivo };
